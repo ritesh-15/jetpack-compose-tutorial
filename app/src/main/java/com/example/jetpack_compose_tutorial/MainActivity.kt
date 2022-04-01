88c8bfacdc4e8fa2861dd016ebe34d7f7268448a
Ritesh Khore <riteshkhore@gmail.com>
2022-04-01 10:47:41 +0530

    Box composibles

--- app/src/main/java/com/example/jetpack_compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_tutorial/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpack_compose_tutorial.ui.theme.JETPACK_COMPOSE_TUTORIALTheme
 import com.example.jetpack_compose_tutorial.ui.theme.Typography
 
@@ -26,16 +32,7 @@ import com.example.jetpack_compose_tutorial.ui.theme.Typography
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    Row(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalArrangement = Arrangement.Start
-                    ) {
 
-                        CustomItem(0.5f)
-
-                        CustomItem(1f)
-
-                    }
                 }
             }
         }
@@ -43,37 +40,26 @@ import com.example.jetpack_compose_tutorial.ui.theme.Typography
 }
 
 
+@Composable
+fun CustomBox(text:String ,color:Color = Color.Blue){
+    Box(modifier = Modifier
+        .background(color)
+        )
+    Text(text = text , fontSize = 40.sp)
+}
 
- @Composable
- fun RowScope.CustomItem(weight: Float,color: Color = MaterialTheme.colors.primary){
-     Surface(
-         modifier = Modifier
-             .width(50.dp)
-             .height(50.dp)
-             .weight(weight)
-         ,
-         color = color) {
-
-     }
- }
-
- 
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JETPACK_COMPOSE_TUTORIALTheme {
         
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-
-            CustomItem(3f)
-
-            CustomItem(1f,MaterialTheme.colors.secondary)
-
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+            contentAlignment =  Alignment.Center
+            ) {
+            CustomBox("Hello there")
         }
         
     }
